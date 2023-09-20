6beba299b14892f1b630c69a1440a632d7942a29
jucasoliveira <jucas.oliveira@gmail.com>
2023-09-20 07:41:55 +0100

    (feature) update fine tunne

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-
 from transformers import T5ForConditionalGeneration, T5Tokenizer, TextDataset, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+import torch
 
 # Initialize the T5 base model and tokenizer
-model = T5ForConditionalGeneration.from_pretrained("t5-small")
+model = T5ForConditionalGeneration.from_pretrained("model")
 tokenizer = T5Tokenizer.from_pretrained("t5-small")
 
-# Prepare the data
-train_dataset = TextDataset(
-    tokenizer=tokenizer,
-    file_path="data/nl2bash-data.json",
-    block_size=512,
-)
-data_collator = DataCollatorForSeq2Seq(
-    tokenizer=tokenizer,
-    model=model,
+# Move model to device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = model.to(device)
+
+# Test the model on a sample input text
+sample_input_text = "Display current running kernel's compile-time config file."
+input_tokenized = tokenizer.encode_plus(
+    sample_input_text,
+    max_length=512,
+    padding='max_length',
+    truncation=True,
+    return_tensors="pt"
 )
 
-# Initialize the Trainer
-training_args = Seq2SeqTrainingArguments(
-    output_dir="./results",
-    overwrite_output_dir=True,
-    num_train_epochs=3,
-    per_device_train_batch_size=32,
-    save_steps=10_000,
-    save_total_limit=2,
-)
-trainer = Seq2SeqTrainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    train_dataset=train_dataset,
+# Move to device
+input_tokenized.to(device)
+
+# Generate output
+output_ids = model.generate(
+    input_ids=input_tokenized["input_ids"].to(device),
+    attention_mask=input_tokenized["attention_mask"].to(device),
+    max_length=50,
+    min_length=5,
+    temperature=1.0
 )
 
-# Fine-tune the model
-trainer.train()
+# Decode and print the output
+decoded_output = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+print("Generated Output:", decoded_output)
